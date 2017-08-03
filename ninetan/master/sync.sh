d6fee22237ce6fb6e97fd7afc8b11989b0951e66
imos <github@imoz.jp>
2017-08-04 07:16:53 +0900

    Mount directories if necessary.

--- ninetan/master/sync.sh
+++ ninetan/master/sync.sh
@@ -4,3 +4,11 @@ set -e -u
 
 rsync -a --delete --exclude='.git' --delete-excluded \
     ~/github/ ~/Dropbox/ICFPC2017/github/
+
+if ! mountpoint /dropbox; then
+  bindfs -p 0777 -u ninetan -g ninetan /home/ninetan/Dropbox/ICFPC2017 /dropbox
+fi
+
+if ! mountpoint /github; then
+  mount --rbind /home/ninetan/github /github
+fi
