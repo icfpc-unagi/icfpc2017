e0432cb71503da0994de67e08e5dc85270c51f9b
imos <github@imoz.jp>
2017-08-04 06:30:00 +0900

    Rename github directory.

--- ninetan/master/poll.sh
+++ ninetan/master/poll.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-cd ~/icfpc2017
+cd ~/github
 git pull
 rsync -a --delete --exclude='.git' --delete-excluded \
-    ~/icfpc2017/ ~/Dropbox/ICFPC2017/github/
+    ~/github/ ~/Dropbox/ICFPC2017/github/
 {
   echo 'HTTP/1.0 200 OK'
   echo 'Content-Type: text/html'
