5a0ed5525a9c9dafb65a3da0c56990b1a21ffbc3
imos <github@imoz.jp>
2017-08-04 07:17:08 +0900

    Write output to /tmp.

--- ninetan/master/poll.sh
+++ ninetan/master/poll.sh
@@ -4,7 +4,8 @@ set -e -u
 
 cd ~/github
 git pull
-bash "$(dirname "${BASH_SOURCE}")/sync.sh" || true
+bash "$(dirname "${BASH_SOURCE}")/sync.sh" \
+    >/tmp/ninetan-sync-out 2>/tmp/ninetan-sync-err || true
 {
   echo 'HTTP/1.0 200 OK'
   echo 'Content-Type: text/html'
