9689c7c3840eca0e6bea776a3dda3c35643e0c0e
imos <github@imoz.jp>
2017-08-04 16:47:32 +0900

    Add sync service for slaves.

---
+++ ninetan/slave/sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e -u
+
+rsync -a --delete --exclude='.git' --delete-excluded master:~/github/ ~/github/
+rsync -a --delete --exclude='.git' --delete-excluded master:~/Dropbox/ICFPC2017 ~/Dropbox/ICFPC2017
+
+sleep 10
