0f566fd481545b5d2636f14c383104bb0546c744
imos <github@imoz.jp>
2017-08-04 16:58:41 +0900

    Add slashes to avoid copying directories.

--- ninetan/slave/sync.sh
+++ ninetan/slave/sync.sh
@@ -3,6 +3,6 @@
 set -e -u
 
 rsync -a --delete --exclude='.git' --delete-excluded master:~/github/ ~/github/
-rsync -a --delete --exclude='.git' --delete-excluded master:~/Dropbox/ICFPC2017 ~/Dropbox/ICFPC2017
+rsync -a --delete --exclude='.git' --delete-excluded master:~/Dropbox/ICFPC2017/ ~/Dropbox/ICFPC2017/
 
 sleep 10
