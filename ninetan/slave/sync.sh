40f29298480e72fded858bc56499d1349b322101
imos <github@imoz.jp>
2017-08-04 17:01:57 +0900

    Mount /dropbox and /github.

--- ninetan/slave/sync.sh
+++ ninetan/slave/sync.sh
@@ -5,4 +5,16 @@ set -e -u
 rsync -a --delete --exclude='.git' --delete-excluded master:~/github/ ~/github/
 rsync -a --delete --exclude='.git' --delete-excluded master:~/Dropbox/ICFPC2017/ ~/Dropbox/ICFPC2017/
 
+if ! mountpoint /dropbox; then
+  sudo mkdir -p /dropbox
+  sudo chmod 0755 /dropbox
+  sudo mount --rbind /home/ninetan/Dropbox/ICFPC2017 /dropbox
+fi
+
+if ! mountpoint /github; then
+  sudo mkdir -p /github
+  sudo chmod 0755 /github
+  sudo mount --rbind /home/ninetan/github /github
+fi
+
 sleep 10
