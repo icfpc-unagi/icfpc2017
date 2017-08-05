2c1bfab75f73533f552eb7e04e8540d2f43bd49a
imos <github@imoz.jp>
2017-08-05 17:43:32 +0900

    Support PHP7.

--- www/library/config.php
+++ www/library/config.php
@@ -4,5 +4,5 @@ date_default_timezone_set('Asia/Tokyo');
 
 require_once(dirname(__FILE__) . '/database.php');
 
-Database::Initialize('52.193.177.29:3306', 'icfpc2016', 'ioJJyZi8');
+Database::Initialize('proxy.sx9.jp:10306', 'unagi', 'kExeDkJP');
 ini_set('memory_limit', '1G');
