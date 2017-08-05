232c6fd9b4f7a7492975f213d4055f7cbb0c6265
imos <github@imoz.jp>
2017-08-06 06:37:12 +0900

    Optimize query

--- www/api/get_battle.php
+++ www/api/get_battle.php
@@ -3,16 +3,16 @@
 require_once(dirname(__FILE__) . '/../library/api.php');
 
 Database::Command('
-    UPDATE battle
-    SET
-      battle_id = (@battle_id := battle_id),
-      battle_lock = NOW() + INTERVAL 5 SECOND
-    WHERE
-      battle_lock <= NOW() AND
-      battle_id IN (
+    SELECT (@battle_id := battle_id) AS battle_id, battle_lock
+    FROM battle NATURAL JOIN (
         SELECT DISTINCT battle_id
-        FROM punter WHERE punter_score IS NULL)
-    ORDER BY battle_lock
+        FROM punter WHERE punter_score IS NULL) AS punter
+    WHERE battle_lock <= NOW()
+    ORDER BY battle_lock DESC LIMIT 1');
+Database::Command('
+    UPDATE battle
+    SET battle_lock = NOW() + INTERVAL 5 SECOND
+    WHERE battle_lock <= NOW() AND battle_id = @battle_id
     LIMIT 1');
 
 if (Database::AffectedRows() != 1) {
