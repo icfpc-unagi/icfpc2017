ec002217580f9be58e10020854258d1b76520c9d
imos <github@imoz.jp>
2017-08-07 15:58:29 +0900

    Bug fix in ranking.

--- www/index.php
+++ www/index.php
@@ -76,10 +76,13 @@ foreach ($battles as $battle) {
   $punters = [];
   foreach ($battle['punters'] as $punter_index => $punter) {
     $punter['punter_index'] = $punter_index;
-    $punters[sprintf('%08d', $punter['punter_score']) .
-             $punter_index] = $punter;
+    $punters[] = $punter;
   }
-  krsort($punters);
+  usort($punters, function($lhs, $rhs) {
+    if ($lhs['punter_score'] > $rhs['punter_score']) return -1;
+    if ($lhs['punter_score'] < $rhs['punter_score']) return 1;
+    return 0;
+  });
   foreach (array_values($punters) as $punter_rank => $punter) {
     $punter['punter_rank'] = $punter_rank;
     $records[$battle['map_id']][$punter['ai_id']][] = $punter;
